4ba8b3921a7b63cc0cfbfb617c4a7f3c4475e018
Shuvo <shovocse753@gmail.com>
2022-09-28 18:34:13 +0600

    Scrollview images margin added

--- day_06/lib/scrollable_view.dart
+++ day_06/lib/scrollable_view.dart
@@ -5,33 +5,40 @@ class ScrollableView extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
-        color: Colors.red,
+        color: Colors.white,
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
+            children: const [
               Image(
                   image: NetworkImage(
                       "https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80")),
+              SizedBox(height: 20),
               Image(
                   image: NetworkImage(
                       "https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80")),
+              SizedBox(height: 20),
               Image(
                   image: NetworkImage(
                       "https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80")),
+              SizedBox(height: 20),
               Image(
                   image: NetworkImage(
                       "https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80")),
+              SizedBox(height: 20),
               Image(
                   image: NetworkImage(
                       "https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80")),
+              SizedBox(height: 20),
               Image(
                   image: NetworkImage(
                       "https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80")),
+              SizedBox(height: 20),
               Image(
                   image: NetworkImage(
                       "https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80")),
+              SizedBox(height: 20),
               Image(
                   image: NetworkImage(
                       "https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80")),
