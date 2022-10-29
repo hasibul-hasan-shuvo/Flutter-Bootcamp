8c628013906bc0a2a3fc8d20dded8758aa971077
Shuvo <shovocse753@gmail.com>
2022-10-29 17:55:41 +0600

    Get request test function created.

--- day_12/lib/network_test.dart
+++ day_12/lib/network_test.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as shuvo;
 
 void main() {
-  getRequestTest();
+  postRequestTest();
 }
 
 void getRequestTest() async {
@@ -14,4 +14,19 @@ void getRequestTest() async {
 
   Map<String, dynamic> json = jsonDecode(response.body);
   print(json["user_agent"]["version"]);
+}
+
+void postRequestTest() async {
+  Map<String, dynamic> userData = {
+    "name": "Shuvo",
+    "job": "Software Engineer",
+  };
+
+  Uri url = Uri.https("reqres.in", "/api/users");
+  Future<shuvo.Response> apiResponse = shuvo.post(
+    url,
+    body: userData,
+  );
+  shuvo.Response response = await apiResponse;
+  print("StatusCode: ${response.statusCode}, \n\nData: ${response.body}");
 }
