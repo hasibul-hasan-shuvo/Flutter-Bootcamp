6ad10ae14abd8b4259e1833433f37bb762d46d9f
Shuvo <shovocse753@gmail.com>
2022-10-29 17:37:06 +0600

    Get request test function created.

--- day_12/lib/network_test.dart
+++ day_12/lib/network_test.dart
@@ -2,7 +2,11 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as shuvo;
 
-void main() async {
+void main() {
+  getRequestTest();
+}
+
+void getRequestTest() async {
   Uri url = Uri.https("ifconfig.co", "json");
   Future<shuvo.Response> apiResponse = shuvo.get(url);
   shuvo.Response response = await apiResponse;
